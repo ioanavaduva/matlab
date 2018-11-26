ced65ec827b65a3cb90ec358f7f835c6f86a095f
ehb18149 <ehb18149@kingston.mathstat.strath.ac.uk>
2018-11-26 14:30:00 +0000

    somecode

---
+++ damped_jacobiM.m
@@ -0,0 +1,14 @@
+function [it, x] = damped_jacobiM(w, x0, A, b, TOL, maxit) 
+M = sparse(diag(diag(A)));
+N = sparse(-(tril(A, -1) + triu(A, 1)));
+it = maxit;
+for i = 1:it
+    x = w*(M\(N*x0) + M\b) + (1-w)*x0;
+    if norm(x-x0, inf) < TOL
+        it = i;
+        break;
+    end
+    x0 = x;
+end
+end
+
