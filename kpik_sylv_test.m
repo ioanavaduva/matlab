fc2e744bb4a3bbe9daeede7ad34a7627935c066d
ehb18149 <ehb18149@miller.mathstat.STRATH.ac.uk>
2019-06-10 16:41:56 +0100

    updated kpik to test against backslash

--- kpik_sylv_test.m
+++ kpik_sylv_test.m
@@ -1,23 +1,21 @@
-nh = 100;
+nh = 50;
 h = 1/nh;
 T = diag(2*ones(nh,1))+diag(-ones(nh-1,1),1)+diag(-ones(nh-1,1),-1);
 I = speye(nh);
 A = -T/(h^2);
-
-%n=nh^2;
 
 B=A;
 
 [LA,UA] = lu(A);
 [LB,UB] = lu(B);
 
-%x = linspace(0,1,nh);
-%y = linspace(1, 0, nh);
-%b = @(x, y) sin(pi.*x).*cos(pi.*y);
-%rhs1 = b(x, y)';
-%rhs2 = rhs1;
-rhs1 = ones(nh, 1);
+x = linspace(0,1,nh);
+y = linspace(1, 0, nh);
+b = @(x, y) sin(pi.*x).*cos(pi.*y);
+rhs1 = b(x, y)';
 rhs2 = rhs1;
+%rhs1 = ones(nh, 1);
+%rhs2 = rhs1;
 
 m = 300;
 tol = 1e-9;
@@ -25,3 +23,13 @@ tol = 1e-9;
 
 fprintf('final true absolute residual norm: \n')
 disp(norm(A*X1*X2'+X1*X2'*B'+rhs1*rhs2'))    %this matrix should never be formed for n large 
+
+% test if we get the same as when using backslash Kronecker form - only do
+% for small problems
+X = X1*X2';
+X = X(:);
+AA = -(kron(A,I)+kron(I,A));
+rhs = rhs1*rhs2';
+rhs = rhs(:);
+U = AA\rhs;
+norm(U-X)
