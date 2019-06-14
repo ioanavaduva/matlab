f2877e7e216fef78513219d881b39c1d81cd764b
qnb15201 <jennifer.pestana@strath.ac.uk>
2019-06-14 14:40:00 +0100

    Added comment to kronprod.m

--- kronprod.m
+++ kronprod.m
@@ -1,5 +1,5 @@
 function [y] = kronprod(A, B, z)
-    Z = vec2mat(z, length(A));
+    Z = vec2mat(z, length(A)); % replace this line by Z = (vec2mat(z, length(A))'; or Z = reshape(z,size(A));
     Y = B*Z*A' + A*Z*B';
     y = Y(:);
 end
