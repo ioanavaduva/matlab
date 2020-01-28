e9e9e940414b878cf60142e344cb47e53a9d8290
Jen Pestana <jennifer.pestana@strath.ac.uk>
2020-01-28 13:19:22 +0000

    Modified zolatorev comparison code

--- MultiRB/ioana_version/zolotarev_vs_logspacepoles.m
+++ MultiRB/ioana_version/zolotarev_vs_logspacepoles.m
@@ -1,5 +1,8 @@
 % rktoolbox Zolotarev third problem vs logspace poles
 clear all
+
+addpath(genpath('../../rktoolbox'));
+addpath(genpath('../../MultiRB'));
 % setup for logspace shifts
 setup_Poisson_rank1rhs;
 
@@ -10,28 +13,31 @@ emax2=eigs(M{2},M{1},1,'LA',opts);
 aa=emin2;
 bb=emax2;
 
-s_parameter = logspace(log10(aa),log10(bb),6);
+
 
 %setup for Zolotarev shifts
-k = 6;      % rational degree
-b = 6;     % sign function on [-10,-1]\cup [1,10]
+k = 12;      % rational degree
+b = emax2-emin2+1;     % sign function on [-10,-1]\cup [1,10]
 r = rkfun.gallery('sign', k/2, b);
+
+s_parameter = logspace(log10(1),log10(b),6);
+
 % Extrema for [-1,-1/b]\cup [1/b,1]:
-K = ellipke(1-1/b^2);
-[sn, cn, dn] = ellipj((0:k)*K/k, 1-1/b^2);
-extrema = b*dn;   % Transplant to [-b,-1]\cup [1,b]
-
-vals = 1-r(extrema);
-c = mean( vals(1:2:end) );
-e = eig( [ 2-4/c^2 1 ; 1 0 ] );
-Zk = min(abs(e))
-
-% Mobius transformation of r(x):
-R = @(x) (1 + (1+Zk)/(1-Zk)*r(x))./(1 - (1+Zk)/(1-Zk)*r(x));
-x = linspace(-b, b, 5000);
-plot(x, R(x), 'linewidth', 2), ylim([-1e4,1e4])
-xlabel('x')
-title('solution to Zolotarev''s third problem'), hold on
-
-semilogy(s_parameter, '-o')
+% K = ellipke(1-1/b^2);
+% [sn, cn, dn] = ellipj((0:k)*K/k, 1-1/b^2);
+% extrema = b*dn;   % Transplant to [-b,-1]\cup [1,b]
+% 
+% vals = 1-r(extrema);
+% c = mean( vals(1:2:end) );
+% e = eig( [ 2-4/c^2 1 ; 1 0 ] );
+% Zk = min(abs(e))
+% 
+% % Mobius transformation of r(x):
+% R = @(x) (1 + (1+Zk)/(1-Zk)*r(x))./(1 - (1+Zk)/(1-Zk)*r(x));
+% x = linspace(-b, b, 5000);
+% plot(x, R(x), 'linewidth', 2), ylim([-1e4,1e4])
+% xlabel('x')
+% title('solution to Zolotarev''s third problem'), hold on
+% 
+% semilogy(s_parameter, '-o')
 
