567d90ed51a7152095635a4e17785a7dde1e752f
ehb18149 <ehb18149@miller.mathstat.strath.ac.uk>
2020-01-28 10:58:41 +0000

    some changes to eq210beckermann.m

--- MultiRB/ioana_version/eq210beckermann.m
+++ MultiRB/ioana_version/eq210beckermann.m
@@ -4,9 +4,7 @@ opts.tol=1e-4;
 emin = 1e-6; 
 emax=eigs(M{2},M{1},1,'LA',opts);
 
-s_parameter = logspace(log10(emin)+1,log10(emax)-1,6);
-
-% u = cell(6,1);
+s_parameter = logspace(log10(emin)+0.1,log10(emax)-0.1,6);
 
 u_inner = @ (z,s) abs((sqrt((z - emax)./(z - emin))*sqrt((s - emax)./(s - emin)) - 1)./((sqrt((z - emax)./(z - emin))*sqrt((conj(s) - emax)./(conj(s) - emin)) + 1)));
 
@@ -16,12 +14,5 @@ u_outer = ones(size(z));
 for i = 1:6
     u_outer = u_outer.*u_inner(z,s_parameter(i));
 end
-    
-% for i = 1:6
-%    u{i} = @(z) abs((sqrt((z - emax)./(z - emin))*(sqrt(s_parameter(i) - emax)./(s_parameter(i) - emin)) - 1)./((sqrt((z - emax)./(z - emin))*(sqrt(s_parameter(i) - emax)./(s_parameter(i) - emin)) + 1)));
-% end
 
-% U =  prod(u);
-% plot(z, U(z), 'linewidth', 2), ylim([-1e4,1e4]);
-
-plot(z, u_outer, 'linewidth', 2), ylim([-1e4,1e4]);
+plot(z, u_outer, 'linewidth', 2), ylim([-1e2,0.7*1e4]);
