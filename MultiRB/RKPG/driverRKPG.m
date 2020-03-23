5e022b8c1b173f392650b1d7b50670a45bceb58a
Ioana Vaduva <36649622+ioanavaduva@users.noreply.github.com>
2020-03-23 16:36:07 +0000

    working denominator poles

--- MultiRB/RKPG/driverRKPG.m
+++ MultiRB/RKPG/driverRKPG.m
@@ -4,7 +4,7 @@ clear all;
 addpath(genpath('../../rktoolbox'));
 
 % Setup
-n = 1000; % size of matrix A
+n = 1200; % size of matrix A
 h = 1/n; eps = 1;
 A = eps*(diag(2*ones(n, 1)) + diag (-1*ones(n-1, 1), 1) + diag (-1*ones(n-1, 1), -1))/h^2;
 rhs1 = ones(n, 1);
@@ -24,16 +24,34 @@ poles_log = logspace(log10(emin), log10(emax), 6)';
 % 4 positive imaginary parts of Zolotarev poles
 bb = emax - emin + 1;
 
-k = 4;      % rational degree
+k = 2;      % rational degree
 b = bb;     % sign function on [-10,-1]\cup [1,10]
 r = rkfun.gallery('sign', k, b);
 % poles(r)
 po = imag(poles(r));
 poles_Zolo = po(po >= 0);
 
+% Compute the minimum of Z Problem 3 (by transforming c, the min of Prob 4)
+K = ellipke(1-1/b^2);
+[sn, cn, dn] = ellipj((0:k)*K/k, 1-1/b^2);
+extrema = b*dn;
+vals = 1-r(extrema);
+c = mean( vals(1:2:end) );
+e = eig( [ 2-4/c^2 1 ; -1 0 ] );
+Zk = min(abs(e));
+
+% Obtain the polynomials p and q of the rational function r = p/q 
+[p,q,pq] = poly(r);
+pp = [0, p];
+
+% Transform Z Problem 4 into Problem 3 using eq. (2) - rearranged in Theorem 2.1
+% (Istace/Thiran paper)and have the denominator given by
+denom = q.*(1-Zk) - pp.*(1+Zk);
+roots_denom = roots(denom);
+
 % time & solve using RKPG
 tic;
-[X1, X2, final_err, vec_res, it, inner_it, avg_inner] = RKPG(A, rhs1, rhs2, poles_Zolo, tol,  maxit);
+[X1, X2, final_err, vec_res, it, inner_it, avg_inner] = RKPG(A, rhs1, rhs2, roots_denom, tol,  maxit);
 time = toc;
 
 
@@ -42,8 +60,8 @@ fprintf('\n Total execution time: %9.4e seconds \n', time)
 fprintf('final_err   avg_inner  \n')
 fprintf('\n  %9.4e       %d    \n \n', [final_err, avg_inner])
 
-% plot residual v iterations
-iter = linspace(1, it+1, it+1);
-semilogy(iter, vec_res, 'o');hold on
-xlabel('Iterations');
+% % plot residual v iterations
+% iter = linspace(1, it+1, it+1);
+% semilogy(iter, vec_res, 'o');hold on
+% xlabel('Iterations');
 
--- MultiRB/RKPG/zolo_pb3.m
+++ MultiRB/RKPG/zolo_pb3.m
@@ -13,31 +13,29 @@ emax = eigs(A, 1,'LA',opts);
 % Compute the solution to Z Problem 4 of rational degree k
 bb = emax - emin + 1;
 
-k = 4;      % rational degree
+k = 2;      % rational degree
 b = bb;     % sign function on [-10,-1]\cup [1,10]
-r = rkfun.gallery('sign', k, b);
+r = rkfun.gallery('sign', k, b);  % Zolotarev's sign approximation on +/-[1, b] 
+    % of type (2*d-1, 2*d) -- this means the degree of p is 2d-1 and the degree of q is 2d.
 
-% Compute the minimum of Z Problem 4
+% Compute the minimum of Z Problem 3 (by transforming c, the min of Prob 4)
 K = ellipke(1-1/b^2);
 [sn, cn, dn] = ellipj((0:k)*K/k, 1-1/b^2);
 extrema = b*dn;
 vals = 1-r(extrema);
 c = mean( vals(1:2:end) );
-e = eig( [ 2-4/c^2 1 ; -1 0 ] );
+e = eig( [ 2-4/c^2 1 ; -1 0 ] ); % changed to have a -1 in one of the off-diagonals
 Zk = min(abs(e));
 
 % Obtain the polynomials p and q of the rational function r = p/q 
-[p,q,pq] = poly(r);
-pp = @(z) polyval(p, z);
-qq = @(z) polyval(q, z);
+[p,q,pq] = poly(r); % p has degree 1 smaller than q, so add a 0 
+% poly gives the coefficients of 3x+1 as vector [3, 1]
+pp = [0, p]; % now p and q have the same degree
 
 % Transform Z Problem 4 into Problem 3 using eq. (2) - rearranged in Theorem 2.1
 % (Istace/Thiran paper)and have the denominator given by
-denom = @(z) qq(z).*(1-Zk) - pp(z).*(1+Zk);
+denom = q.*(1-Zk) - pp.*(1+Zk);
+roots_denom = roots(denom);
 
-%%%% Questions: What should I make z in order to find the roots of denom?
-%%%% Should it be the same as k, i.e. the degree of the rational polynomial?
 
-z = linspace(-b, b, k);
-poly(denom(z)) % these values are very large. what am I doing wrong??
 
