ccf8ff603527aa9646b174935f28e2bd8dc71790
Ioana Vaduva <36649622+ioanavaduva@users.noreply.github.com>
2020-03-30 16:15:52 +0100

    zolotarev poles tests

--- MultiRB/RKPG/driverRKPG.m
+++ MultiRB/RKPG/driverRKPG.m
@@ -4,14 +4,14 @@ clear all;
 addpath(genpath('../../rktoolbox'));
 
 % Setup
-n = 100; % size of matrix A
+n = 800; % size of matrix A
 h = 1/n; eps = 1;
 A = eps*(diag(2*ones(n, 1)) + diag (-1*ones(n-1, 1), 1) + diag (-1*ones(n-1, 1), -1))/h^2;
 rhs1 = ones(n, 1);
 rhs2 = ones(n, 1);
 
-tol = 1e-10;
-maxit = 23;
+tol = 1e-9;
+maxit = 100;
 
 % Get smallest and largest eigenvalues
 emin = 1e-6; 
@@ -24,7 +24,7 @@ poles_log = logspace(log10(emin), log10(emax), 6)';
 % 4 positive imaginary parts of Zolotarev poles
 bb = emax - emin + 1;
 
-k = 4;      % rational degree
+k = 1;      % rational degree
 b = bb;     % sign function on [-10,-1]\cup [1,10]
 r = rkfun.gallery('sign', k, b);
 % poles(r)
@@ -32,26 +32,26 @@ po = imag(poles(r));
 poles_Zolo = po(po >= 0);
 
 % % Compute the minimum of Z Problem 3 (by transforming c, the min of Prob 4)
-% K = ellipke(1-1/b^2);
-% [sn, cn, dn] = ellipj((0:k)*K/k, 1-1/b^2);
-% extrema = b*dn;
-% vals = 1-r(extrema);
-% c = mean( vals(1:2:end) );
-% e = eig( [ 2-4/c^2 1 ; -1 0 ] );
-% Zk = min(abs(e));
-% 
-% % Obtain the polynomials p and q of the rational function r = p/q 
-% [p,q,pq] = poly(r);
-% pp = [0, p];
-% 
-% % Transform Z Problem 4 into Problem 3 using eq. (2) - rearranged in Theorem 2.1
-% % (Istace/Thiran paper)and have the denominator given by
-% denom = q.*(1-Zk) - pp.*(1+Zk);
-% roots_denom = roots(denom);
+K = ellipke(1-1/b^2);
+[sn, cn, dn] = ellipj((0:k)*K/k, 1-1/b^2);
+extrema = b*dn;
+vals = 1-r(extrema);
+c = mean( vals(1:2:end) );
+e = eig( [ 2-4/c^2 1 ; -1 0 ] );
+Zk = min(abs(e));
+
+% Obtain the polynomials p and q of the rational function r = p/q 
+[p,q,pq] = poly(r);
+pp = [0, p];
+
+% Transform Z Problem 4 into Problem 3 using eq. (2) - rearranged in Theorem 2.1
+% (Istace/Thiran paper)and have the denominator given by
+denom = q.*(1-Zk) - pp.*(1+Zk);
+roots_denom = roots(denom);
 
 % time & solve using RKPG
 tic;
-[X1, X2, final_err, vec_res, it, inner_it, avg_inner] = RKPG(A, rhs1, rhs2, poles_Zolo, tol,  maxit);
+[X1, X2, final_err, vec_res, it, inner_it, avg_inner] = RKPG(A, rhs1, rhs2, roots_denom, tol,  maxit);
 time = toc;
 
 
--- MultiRB/RKPG/zolo_pb3.m
+++ MultiRB/RKPG/zolo_pb3.m
@@ -2,7 +2,7 @@ clear all;
 addpath(genpath('../../rktoolbox'));
 
 % Setup
-n = 1000; % size of matrix A
+n = 100; % size of matrix A
 h = 1/n; eps = 1;
 A = eps*(diag(2*ones(n, 1)) + diag (-1*ones(n-1, 1), 1) + diag (-1*ones(n-1, 1), -1))/h^2;
 
@@ -13,7 +13,7 @@ emax = eigs(A, 1,'LA',opts);
 % Compute the solution to Z Problem 4 of rational degree k
 bb = emax - emin + 1;
 
-k = 2;      % rational degree
+k = 4;      % rational degree
 b = bb;     % sign function on [-10,-1]\cup [1,10]
 r = rkfun.gallery('sign', k, b);  % Zolotarev's sign approximation on +/-[1, b] 
     % of type (2*d-1, 2*d) -- this means the degree of p is 2d-1 and the degree of q is 2d.
@@ -37,5 +37,16 @@ pp = [0, p]; % now p and q have the same degree
 denom = q.*(1-Zk) - pp.*(1+Zk);
 roots_denom = roots(denom);
 
+po = imag(poles(r));
+poles_Zolo = po(po >= 0);
+
+% Plot the solution of the third Zolotarev problem (from rktoolbox) and the
+% new poles obtained from the roots of the denominator
+R = @(x) (1 + (1+Zk)/(1-Zk)*r(x))./(1 - (1+Zk)/(1-Zk)*r(x));
+x = linspace(-b, b, 10000);
+plot(x, R(x), 'linewidth', 2), ylim([-1e2,1e2])
+xlabel('x')
+title('solution to Zolotarev''s third problem'); hold on;
+plot(roots_denom, zeros(length(roots_denom)), 'o'); hold off
 
 
