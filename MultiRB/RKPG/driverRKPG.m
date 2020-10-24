0d8243a34eb264fe1204b11357ecf35b6f7281c2
Ioana Vaduva <36649622+ioanavaduva@users.noreply.github.com>
2020-10-24 15:19:12 +0100

    updates to code

--- MultiRB/RKPG/driverRKPG.m
+++ MultiRB/RKPG/driverRKPG.m
@@ -1,11 +1,10 @@
-
-% Driver for RKPG (with 2-sided projection)
+%% Driver for RKPG (with 2-sided projection)
 
 clear all;
 addpath(genpath('../../rktoolbox'));
 
-% Setup
-n = 500; % size of matrix A 
+%% Setup
+n = 100; % size of matrix A 
 h = 1/n; eps = 1;
 A = eps*(diag(2*ones(n, 1)) + diag(-1*ones(n-1, 1), 1) + diag(-1*ones(n-1, 1), -1))/h^2;
 A_rksm = -(eps*(diag(2*ones(n, 1)) + diag(-1*ones(n-1, 1), 1) + diag(-1*ones(n-1, 1), -1))/h^2);
@@ -23,41 +22,59 @@ I = speye(n);
 % end
 %%%
 
-% rhs1 = I(:,6); rhs2=rhs1;
+%%%% Choose RHS
 
+%% Identity
+% rhs1 = I(:,6); rhs2=rhs1;
+%% Polynomial
+% xtemp = linspace(0,1,n);
+% x = repmat(xtemp, 1, n);
+% y = reshape(repmat(xtemp, length(xtemp), 1), 1, length(xtemp)^2);
+% b = @(x, y) -2.*(6.*x.^2 - 6.*x + 1).*(y-1).^2.*y.^2-2.*(x-1).^2.*x.^2.*(6.*y.^2-6.*y+1);
+% rhs = b(x, y)';
+% rhs11 = reshape(rhs, n, n); rhs1 = rhs11(:,1);
+% rhs2=rhs1;
+%% Sin(pi*x)Sin(pi*y) 
+% xtemp = linspace(0,1,n);
+% x = repmat(xtemp, 1, n);
+% y = reshape(repmat(xtemp, length(xtemp), 1), 1, length(xtemp)^2);
+% b = @(x, y) sin(pi*x).*sin(pi*y);
+% rhs = b(x, y)';
+% rhs11 = reshape(rhs, n, n); rhs1 = rhs11(:,1);
+% rhs2=rhs1;
+%% Ones
 % rhs1 = ones(n, 1);
 % rhs2 = ones(n, 1);
-
+%% Random from the standard normal distribution
 rhs1 = randn(n, 1);
 rhs2 = rhs1;
-
+%% Another random
 % NNn = 0.0001;
 % rhs1 = NNn*randn(n, 1) + ones(n, 1);
 % rhs2 = rhs1;
-
+%% Linspace
 % rhs1 = linspace(1, n, n)'; rhs2 = rhs1;
-
+%% Cos(pi*x)
 % x = linspace(0, 1, n)';
 % rhs1 = cos(pi*x); rhs2 = rhs1;
-
+%% Another random
 % rhs1 = sprand(n,1,0.23); rhs2 = rhs1;
- 
+%% Alternate 1&-1
 % rhs1 = ((-1).^(0:n-1))'; rhs2 = rhs1;
 
-% %%% Exact solution --- only need to check bounds for the 3 bases & to
-% %%%% compare with 1 sided projection
+%% Exact solution --- only need to check bounds for the 3 bases & to
+%%%% compare with 1 sided projection
 % AA = kron(A, speye(n))+kron(speye(n), A);
 % rhs = rhs1*rhs2';
 % rhss = rhs(:);
 % Xex = AA\rhss;
 % Xex_mat = reshape(Xex, n, n);
-% %%%%
+%%%%
 
 tol = 1e-9;
 maxit = 300;
 
-% Get smallest and largest eigenvalues
-% emin = 1e-6; 
+%% Get smallest and largest eigenvalues
 opts.tol=1e-4;
 emin = eigs(A, 1, 'smallestabs', opts);
 emax = eigs(A, 1,'largestabs',opts);
@@ -77,15 +94,14 @@ emax_rksm = eigs(A_rksm, 1,'smallestabs',opts);
 % snew = get_nodes2(emin,emax,s_nodes);      % Use interval for A_1;
 % s_parameter=snew;
 
-% 4 positive imaginary parts of Zolotarev poles
+%% Zolotarev
 bb = emax - emin + 1;
 k = 4;      % number of poles is 2*k
 roots_denom = get_rootsden(k, bb) + emin - 1;
 
-m = 12; % number of poles; can change
+%% IRKA
+m = 14; % number of poles; can change
 xi = emin + (emax-emin)*rand(1,m);
-% 
-% IRKA shifts
 [shifts,its] = irka_shifts(A,rhs1, xi, 1e-4);
 
 % %%% work with 6 poles only
@@ -119,15 +135,17 @@ xi = emin + (emax-emin)*rand(1,m);
 % pol = [9.6744; 200.0000; 2.0205e+04; 2.8462e+04];
 % greedy poles n=500
 % pol = [9.83021189253562; 1000.00000000001; 9.83021189253562; 37834.7018953791; 9.83021189253562; 9.83021189253562; 9.83021189253562; 9.83021189253562; 9.83021189253562; 9.83021189253562; 284713.686991583];
+%% Adaptive (Druskin & Simoncini)
 [Z, nrmrestot, pol] = rksm(A_rksm, I, I, rhs1, maxit, tol, emin, emax_rksm, 0, 1e-12);
 [pols,m1,n1] = uniquetol(pol);
 [c1,d1] = sort(m1);
 pols_dist = pols(d1);
 fprintf('\n Number of rksm unique poles: %d \n', length(pols_dist))
 fprintf('\n Number of rksm poles (total): %d \n', length(pol))
-% time & solve using RKPG
+
+%% time & solve using RKPG
 tic;
-[X1, X2, final_err, vec_res, it, inner_it, avg_inner, upper_vec] = RKPG(A, rhs1, rhs2, pol, tol,  maxit);
+[X1, X2, final_err, vec_res, it, inner_it, avg_inner, upper_vec] = RKPG(A, rhs1, rhs2, pols_dist, tol,  maxit);
 %!! for beckermann bound need to add extra 'upper_bound' to outputs
 %!! to check errors need error_vec in outputs and Xex_mat (exact solution in matrix form) in inputs
 %!! plot Ritz values need e_Ap in outputs
@@ -138,9 +156,9 @@ fprintf('\n Total execution time: %9.4e seconds \n', time)
 fprintf('final_err   avg_inner  \n')
 fprintf('\n  %9.4e       %d    \n \n', [final_err, avg_inner])
 
-% plot residual v iterations
+%% plot residual v iterations
 iter = linspace(1, it, it);
-semilogy(iter, vec_res, 's');hold on
+semilogy(iter, vec_res, 'v');hold on
 xlabel('Iterations');
 ylabel('Residual');
 % plot(error_vec, 'o');hold on;
